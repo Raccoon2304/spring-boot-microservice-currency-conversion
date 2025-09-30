536ddd3967c84954216880b542f2e75982222e21
abelousov <abelousov@lanit.ru>
2025-09-30 17:25:30 +0400

    initial commit

---
+++ src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/CurrencyConversionBean.java
@@ -0,0 +1,23 @@
+package com.in28minutes.springboot.microservice.example.currencyconversion;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversionBean {
+
+  private Long id;
+  private String from;
+  private String to;
+  private BigDecimal conversionMultiple;
+  private BigDecimal quantity;
+  private BigDecimal totalCalculatedAmount;
+  private int port;
+
+}
